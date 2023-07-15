b5873603d4451a16ee02ec223ff175cf95375b11
Shubham Joshi <sjoshi91119@gmail.com>
2023-07-15 16:21:13 +0530

    asd

--- src/components/SignupPage.js
+++ src/components/SignupPage.js
@@ -2,10 +2,10 @@ function SignupPage() {
   return (
     <>
       <div className="container1">
-        <img src="/src/images/figure.png"></img>
         <div className="form-box"></div>
       </div>
     </>
   );
 }
+
 export default SignupPage;
