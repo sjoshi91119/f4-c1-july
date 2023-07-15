ebf22eff68f5b82fcdbbf2050c83962d93882367
Shubham Joshi <sjoshi91119@gmail.com>
2023-07-15 16:58:33 +0530

    sdfsdf

--- src/components/SignupPage.js
+++ src/components/SignupPage.js
@@ -1,7 +1,10 @@
+import Abstraction from "../images/Abstraction.svg";
+
 function SignupPage() {
   return (
     <>
       <div className="container1">
+        <img src={Abstraction} alt="Abstraction's Pic" />
         <div className="form-box"></div>
       </div>
     </>
