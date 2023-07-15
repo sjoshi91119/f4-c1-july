c506d690146b2b9de30180cc4f81185aa55d865b
Shubham Joshi <sjoshi91119@gmail.com>
2023-07-15 18:12:11 +0530

    idk

--- src/components/SignupPage.js
+++ src/components/SignupPage.js
@@ -1,11 +1,23 @@
 import Abstraction from "../images/Abstraction.svg";
-
+import Facebook from "../images/Facebook.svg";
+import Google from "../images/Google.svg";
 function SignupPage() {
   return (
     <>
       <div className="container1">
         <img src={Abstraction} alt="Abstraction's Pic" />
-        <div className="form-box"></div>
+
+        <div className="form-box">
+          <form>
+            <h1>Create Account</h1>
+            <button className="btn1">
+              <img src={google} alt="google's Pic" /> Signup With Google
+            </button>
+            <button className="btn1">
+              <img src={fb} alt="fb's Pic" /> Signup With Facebook
+            </button>
+          </form>
+        </div>
       </div>
     </>
   );
